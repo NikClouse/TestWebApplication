93b79205d09bf7d3b6992f3a4b2cdb4fc5338193
murod99_99 <murod99_99@bk.ru>
2023-07-07 14:30:30 +0300

    Add project files.

---
+++ TestWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestWebApplication.Models;
+
+namespace TestWebApplication.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		public IActionResult Index()
+		{
+			Test model = new Test
+			{
+				LinkText = "Запустить",
+				ExecutablePath = "notepad.exe"
+			};
+			return View(model);
+		}
+
+		// Событие onClick для гиперссылки
+		public IActionResult RunExecutable(string executablePath)
+		{
+			Process.Start(executablePath);
+			return RedirectToAction("Index");
+		}
+
+
+
+
+
+	}
+}
---
+++ TestWebApplication/Models/Test.cs
@@ -0,0 +1,8 @@
+﻿namespace TestWebApplication.Models
+{
+	public class Test
+	{
+		public string LinkText { get; set; }
+		public string ExecutablePath { get; set; }
+	}
+}
